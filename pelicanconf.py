0c5e92444bbbc5142cebd3a4eba69c9ff826edb4
Jean-Philippe Evrard <jean-philippe@evrard.me>
2016-06-13 20:51:54 +0100

    Configuration in tree
    Signed-off-by: Jean-Philippe Evrard <jean-philippe@evrard.me>

---
+++ pelicanconf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jean-Philippe Evrard'
+AUTHOR_GENERIC_USERNAME = 'evrardjp'
+SITENAME = u'dd if=/dev/brain of=this.site'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+        )
+
+# Social widget
+LINKEDIN_USER = AUTHOR_GENERIC_USERNAME
+LINKEDIN_URL = 'https://www.linkedin.com/in/' + LINKEDIN_USER
+GITHUB_URL = 'https://github.com/' + AUTHOR_GENERIC_USERNAME
+SOCIAL = (('github', LINKEDIN_URL),
+          ('linkedin', GITHUB_URL),)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['images', 'pdfs']
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+#THEME = 'themes/twenty-pelican-html5up'
+#ABOUT_TEXT = 'TODO!'
+#ABOUT_IMAGE = TODO
+#ABOUT_LINK = TODO
+COPYRIGHT = AUTHOR
+#SHOW_COPYRIGHT : True by default, you can set it to False to hide the copyrights.
